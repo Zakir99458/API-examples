e46c095027a08679e2acbc05551a750031533087
Mohammad Zakir Hossain <engineer.zakir.hossain@gmail.com>
2023-12-26 16:48:45 +0200

    food-added

--- js/mealDb.js
+++ js/mealDb.js
@@ -2,12 +2,33 @@ const searchFood = () =>{
     const searchField = document.getElementById('search-field');
     const itemName = searchField.value;
     searchField.value = '';
-    console.log(itemName);
+    // console.log(itemName);
 
     const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${itemName}`;
-    
+
     fetch(url)
     .then(response => response.json())
-    .then(data => console.log(data))
+    .then(data => displaySearchedFood(data.meals))
+}
 
+const displaySearchedFood = (foods) => {
+    const searchResult = document.getElementById('search-result');
+
+    foods.forEach(food => {
+        const div = document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML = `
+            <div class="card">
+                <img src="${food.strMealThumb}" class="card-img-top" alt="image not found">
+                <div class="card-body">
+                <h5 class="">${food.strMeal}</h5>
+                <p class="card-text">${food.strInstructions.slice(0,200)}</p>
+                </div>
+            </div>
+        `
+        console.log("fish", food);
+        searchResult.appendChild(div);
+    });
+    
+    // console.log(foods)
 }
